cfd498eefbeed0bf2b9ad840407dd91ae5336639
Mikunu <nonamebeautifultrap@gmail.com>
2024-02-28 19:46:11 +0600

    added equals/hashCode/toString

--- src/main/java/web/models/Car.java
+++ src/main/java/web/models/Car.java
@@ -1,5 +1,7 @@
 package web.models;
 
+import java.util.Objects;
+
 public class Car {
     private Long id;
     private String label;
@@ -33,5 +35,27 @@ public class Car {
 
     public void setAmount(int amount) {
         this.amount = amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return amount == car.amount && Objects.equals(id, car.id) && Objects.equals(label, car.label);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, label, amount);
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", label='" + label + '\'' +
+                ", amount=" + amount +
+                '}';
     }
 }
